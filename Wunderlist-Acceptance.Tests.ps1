b156317523e1a4b90d12c3dc87afcc1dcb6d7fa8
Jakub Jareš <me@jakubjares.com>
2015-06-28 01:46:14 +0200

    Make the acceptance tests work for anyone who is authenticated

---
+++ Wunderlist-Acceptance.Tests.ps1
@@ -0,0 +1,33 @@
+﻿# these Pester tests are acceptance tests, we are making sure the way we communicate
+# with the real service are correct, so you need to be authenticated with the web service
+
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+Import-Module -Force "$here\Wunderlist.psd1"
+
+Describe "Get-WunderListList" {
+    It 'Inbox WunderList List is retrived correctly' {
+        # -- Arrange
+        $expected = $listName = 'Inbox'
+        	
+        # -- Act
+        $lists = Get-WunderlistList
+        $inboxList = $lists | where {$_.title -eq $listName} 
+        $actual = $inboxList.Title
+
+        # -- Assert
+        $actual | Should Be $expected 
+    }
+}
+
+Describe 'Get-WunderListUser' {
+    It 'Outputs username correctly' {
+    	# -- Arrange
+    	
+    	# -- Act
+        $user = Get-WunderlistUser
+        $actual = $user.Name 
+    	
+    	# -- Assert
+        $actual | Should Not BeNullOrEmpty 
+    }
+}
